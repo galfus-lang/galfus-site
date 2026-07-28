7db751ea5b2cf7b0b2b17dd1a3eb50420c474910
Renato Rodrigues <renato@vulppi.dev>
2026-07-28 10:28:30 -0300

    feat: integrate Google Tag Manager into site layout for production environments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,14 @@
 
   import '$lib/styles/global.css';
 
+  import GTM from '$lib/seo/GTM.svelte';
+  import { dev } from '$app/environment';
+
   let { children } = $props();
 </script>
+
+{#if !dev}
+  <GTM />
+{/if}
 
 {@render children()}
